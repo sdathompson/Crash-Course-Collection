06b5b3e9ba54986bf5d9aa093a9c32089c9346fe
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2024-11-29 16:27:09 -0500

    Chuck Norris Backend
    -async/await added
    -comments added
    -Site works as intended

--- JS Crash Course/Tutorials/Async/Chuck Norris/chucknorris.js
+++ JS Crash Course/Tutorials/Async/Chuck Norris/chucknorris.js
@@ -1,12 +1,17 @@
 const loadJoke = async () => {
+   // Call the try-catch method and add the header to accept the .json
     try{
-        const chuckNorrisFetch = await ('https://api.chucknorris.io/jokes/random', {
+        //Await the fetch of the API
+        const chuckNorrisFetch = await fetch('https://api.chucknorris.io/jokes/random', {
             headers: {
+                // Headers section accept the .JSON app data
                 Accept: "application/json"
             }
         });
 
+        // Fetch the .JSON from the data provided
         const jokeData = await chuckNorrisFetch.json();
+        // Get the value from the joke data to display in the HTML
         document.getElementById('loadingJoke').innerHTML = jokeData.value;
     } 
     catch(error) {
@@ -14,3 +19,5 @@ const loadJoke = async () => {
     }
 }
 
+//To get the joke, target the button to load the async function to get a random joke for us
+document.getElementById('loadJokeBtn').addEventListener("click", loadJoke);
