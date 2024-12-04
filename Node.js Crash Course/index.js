0a1057ef4f0f7505bb7195b6b25b61a2755557a5
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2024-12-04 11:14:04 -0500

    Post Controller import/export example
    - remember to use square brackets with arrays of objects

--- Node.js Crash Course/index.js
+++ Node.js Crash Course/index.js
@@ -1,3 +1,6 @@
-const { generateRandomNumber, celsiusToFahrenheit } = require('./utils');
+// Old syntax
+// const { generateRandomNumber, celsiusToFahrenheit } = require('./utils');
 
-console.log(`C to F: ${celsiusToFahrenheit(generateRandomNumber())}.`)
+// console.log(`C to F: ${celsiusToFahrenheit(generateRandomNumber())}.`)
+
+import { getPost } from './postController.js';
---
+++ Node.js Crash Course/postController.js
@@ -0,0 +1,7 @@
+// This is where Mongo.db would be used to hold the data
+const posts = [
+    { id: 1, title: 'Post One' },
+    { id: 2, title: 'Post Two' }
+];
+
+export const getPosts = () => posts;
