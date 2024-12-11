c790efbdf3c41c661d2fa62cb2c65be69bac4447
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2024-12-11 10:03:22 -0500

    Node Server
    How to run a basic server
    -res.write, res.writeHead, and res.end commands
    - editing the .json file to support the start of a server with the npm module
    (npm start)

--- Node.js Crash Course/server.js
+++ Node.js Crash Course/server.js
@@ -1,7 +1,17 @@
 //Module included with node.js
 import http, { request } from 'http';
+const PORT = 8000;
 
 const server = http.createServer((req, res) => {
+    // Method to set Header content type to html
+    // Will use HTML tags
+    res.writeHead(500, {'Content-Type': 'application/json'});
+    // When using express, you don't have implement res.end()
+    // Sending information to the client (i.e. browser)
+    res.end('<h1>Hello World</h1>');
+}); 
 
-}) 
+server.listen(PORT,() => {
+    console.log(`Server on port port ${PORT}`);
+});
 
