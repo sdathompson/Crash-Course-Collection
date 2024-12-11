baf968c2d35f55435dc99f38dece5a7a6bf7e5eb
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2024-12-11 12:05:44 -0500

    .env PORT info and Loading HTML files to server
    - .env on .gitignore (sensitive info)
    - loading HTML files from node server (import methods)

--- Node.js Crash Course/server.js
+++ Node.js Crash Course/server.js
@@ -1,14 +1,46 @@
 //Module included with node.js
 import http, { request } from 'http';
-const PORT = 8000;
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+
+// Get current path with file
+const __filename = url.fileURLToPath(import.meta.url);
+// Get current path directory (no .js file)
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+const server = http.createServer(async (req, res) => {
     // Method to set Header content type to html
     // Will use HTML tags
-    res.writeHead(500, {'Content-Type': 'application/json'});
-    // When using express, you don't have implement res.end()
-    // Sending information to the client (i.e. browser)
-    res.end('<h1>Hello World</h1>');
+    try {
+        //To check if it's a GET request
+        if (req.method === 'GET') {
+            let filePath;
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if (req.url === '/about') {        
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {        
+                throw new Error('Method not allowed');
+            }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+            
+        } else {
+            throw new Error('Method not allowed');
+        }
+        
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Server Error');
+        
+    }
+
 }); 
 
 server.listen(PORT,() => {
