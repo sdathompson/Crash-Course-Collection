acec2c58a55e52145cac59ce4bc3d6f71803b5a1
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2024-12-18 13:07:48 -0500

    Send Files through sendFile() and middleware
    -added the path module
    -send the file through .sendFile(__dirname)
    -different more efficient method through static middleware (set the folder as the base path)

--- Express Crash Course/server.js
+++ Express Crash Course/server.js
@@ -1,16 +1,34 @@
 //Syntax to initialize express in a file
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => { 
-    // Don't have to define the content type. Automatically update to the desired content
-    res.send({message: 'Hello World'});
-    res.end();
+// setup static folder (public)
+// Middleware is a function that runs between the incoming request 
+//and the outgoing response.
+// This static middleware finds the path of the folder and 
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//Pseudo Database
+let posts = [
+    {id: 1, title: "Post One"},
+    {id: 2, title: "Post Two"},
+    {id: 3, title: "Post Three"}
+]
+
+
+app.get('/api/posts', (req, res) => {
+
 });
 
-app.get('/about', (req, res) => {
-    res.send('About');
-});
+// app.get('/', (req, res) => { 
+//     // Sending the HTML file in the public/index.html dir path
+//    res.sendFile(path.join(__dirname,  'public', 'index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname,'public', 'about.html'));
+// });
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
