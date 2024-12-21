90bddbe88a7b854ed89ef2b6468821da82c1d480
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2024-12-20 22:18:59 -0500

    Adding a post from a user filled form

--- Express Crash Course/public/js/main.js
+++ Express Crash Course/public/js/main.js
@@ -1,5 +1,6 @@
 const output = document.querySelector('#output');
 const button = document.querySelector('#get-posts-btn');
+const form = document.querySelector('#add-post-form');
 
 // Get and show posts
 async function showPosts() {
@@ -10,10 +11,11 @@ async function showPosts() {
             throw new Error('Failed to fetch posts');
         }
         
-        // await body of the post
+        // await body of the post (stringified JSON)
         const posts = await res.json();
         output.innerHTML = '';
-    
+        
+        //iterate through posts array
         posts.forEach((post) => {
             // Create an element for each post
             const postEle = document.createElement('div');
@@ -25,5 +27,40 @@ async function showPosts() {
     }
 }
 
+// Submit new post
+async function addPost(event) {
+    // Prevent the post from being added if there's nothing in the field
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const title = formData.get('title');
+
+    try {
+        // fetch the API with an optional method to POST stringified JSON text 
+        const res = await fetch('http://localhost:8000/api/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({title})
+        });
+
+        //Null check for post
+        if (!res.ok) {
+            throw new Error('Failed to add post');
+        }
+
+        const newPost = await res.json();
+
+        const postEle = document.createElement('div');
+        postEle.textContent = newPost.title;
+        output.appendChild(postEle);
+        showPosts();
+    } catch (error) {
+        console.error('Error adding post');
+    }
+}
+
 // Event listeners: button click to show Posts
 button.addEventListener('click', showPosts);
+form.addEventListener('submit', addPost);
