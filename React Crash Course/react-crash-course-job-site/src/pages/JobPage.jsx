cffc5cafcd50e20d7a5432d7172c497b47753ab3
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2025-01-13 09:05:48 -0500

    Creating a page for each single job listing
    -Solved bug in JobListing.jsx where it was displaying one-time IDs. In a dynamic element ${} is needed in ``

---
+++ React Crash Course/react-crash-course-job-site/src/pages/JobPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+
+const JobPage = () => {
+    //useParams to get the id for the fetch api
+    const { id } = useParams();
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => { 
+        const fetchJob = async() => {
+            try {
+                const res = await fetch(`/api/jobs/${id}`);
+                const data = await res.json();
+                console.log(data);
+                setJob(data);        
+              } catch (error) {
+                  console.log('Error fetching data: ', error);
+              } finally {
+                setLoading(false);
+              }
+        };
+
+        fetchJob();
+    }, [])
+    
+  return loading ? <Spinner /> : (
+    <h1>{ job.title }</h1>
+  );
+};
+
+export default JobPage
