5eaa76fb9240a27a504bb77760300790dd7069a3
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2025-01-13 11:21:47 -0500

    Data Loaders

--- React Crash Course/react-crash-course-job-site/src/pages/JobPage.jsx
+++ React Crash Course/react-crash-course-job-site/src/pages/JobPage.jsx
@@ -1,34 +1,40 @@
 import React from 'react'
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
+import { useParams, useLoaderData } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 
 const JobPage = () => {
     //useParams to get the id for the fetch api
     const { id } = useParams();
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const job = useLoaderData();
+    // const [job, setJob] = useState(null);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() => { 
-        const fetchJob = async() => {
-            try {
-                const res = await fetch(`/api/jobs/${id}`);
-                const data = await res.json();
-                console.log(data);
-                setJob(data);        
-              } catch (error) {
-                  console.log('Error fetching data: ', error);
-              } finally {
-                setLoading(false);
-              }
-        };
+    // useEffect(() => { 
+    //     const fetchJob = async() => {
+    //         try {
+    //             const res = await fetch(`/api/jobs/${id}`);
+    //             const data = await res.json();
+    //             console.log(data);
+    //             setJob(data);        
+    //           } catch (error) {
+    //               console.log('Error fetching data: ', error);
+    //           } finally {
+    //             setLoading(false);
+    //           }
+    //     };
 
-        fetchJob();
-    }, [])
+    //     fetchJob();
+    // }, [])
     
   return loading ? <Spinner /> : (
     <h1>{ job.title }</h1>
   );
 };
 
-export default JobPage
+const jobLoader = async({params}) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
+}
+export { JobPage as default, jobLoader };
