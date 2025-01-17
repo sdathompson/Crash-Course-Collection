b0a313a4e02bf3b84f937a35c09c9e0aee3b0e16
Shane Thompson <78186722+Miidniie@users.noreply.github.com>
2025-01-17 18:01:31 -0500

    Starting the Python Grind!

---
+++ Python Crash Course/README.md
@@ -0,0 +1,30 @@
+
+# Python Crash Course
+
+This project is a collection of Python crash course materials and exercises.
+
+## Setup
+
+1. Create a virtual environment:
+    ```sh
+    python -m venv venv
+    ```
+
+2. Activate the virtual environment:
+    - On Windows:
+        ```sh
+        .\venv\Scripts\activate
+        ```
+    - On macOS and Linux:
+        ```sh
+        source venv/bin/activate
+        ```
+
+3. Install dependencies:
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+Describe how to use your project here.
---
+++ Python Crash Course/quicktour.py
@@ -0,0 +1,52 @@
+# # This is my first python program
+# print('I like sushi!')
+
+# first_name = 'Shane'
+# food = 'sushi'
+# email = 'test@test.com'
+
+# # Strings
+# print(f'Hello, {first_name}!')
+# print (f'You like {food}!')
+# print(f'Your email is: {email}!')
+
+# # Integers
+# age = 27
+# quantity = 3
+# num_of_students = 30
+
+# print(f'You are {age} years old!')
+# print(f'You buying {quantity} {food}')
+# print(f'There are {num_of_students} students in the class')
+
+# Boolean
+
+# is_student = True
+# for_sale = True
+# is_online = True
+
+# if is_student:
+#     print('You are a student')
+# else:
+#     print('You are not a student')
+
+# if for_sale:
+#     print(f'{food} is for sale')
+# else:
+#     print(f'{food} is not for sale')
+
+# if is_online:
+#     print('{name} is online')
+# else:
+#     print('{name} is offline')
+
+# Python Typecasting
+gpa = 3.7
+
+
+gpa = int(gpa) #output = 3
+
+# Input() prompts the user to return something in the CLI
+
+name = input('What is your name?: ')
+# print(f'Hello, {name}!') #output = What's your name?: input , Hello input!
