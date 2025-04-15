649abc752a8ea360180c2ae16d5e091061b5fd8d
Shane Thompson <sda.thompson@hotmail.com>
2025-04-15 15:45:21 -0400

    If/Else
    -draw.io flowchart

--- Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 3/If Else/task.py
+++ Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 3/If Else/task.py
@@ -1,2 +1,7 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print ("You are tall enough to ride this rollercoaster.")
+else:
+    print("You aren't tall enough to ride this rollercoaster.")
