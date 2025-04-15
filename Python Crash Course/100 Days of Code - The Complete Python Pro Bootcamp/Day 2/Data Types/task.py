1b8b09a30546efe19f4bf73d65c8765323b1ab7c
Shane Thompson <sda.thompson@hotmail.com>
2025-04-15 13:20:52 -0400

    2nd Day Halfway Done

--- Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Data Types/task.py
+++ Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Data Types/task.py
@@ -1,1 +1,20 @@
-len("Hello")
+print(len("Hello"))
+
+# String - characters
+# Subscripting
+print("Hello"[-1])
+
+#Integers - whole numbers
+print(123 + 345)
+
+#Large Integers - if you want commas, use _
+print(123_456_789)
+
+#Float - Short for floating point number; decimal places
+print (123.456)
+
+#Boolean - two possible values, True(1) or False(0)
+print(True)
+print(False)
+
+
--- Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
+++ Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,9 @@
-len(12345)
+len("12345")
+
+print(type("Hello"))
+print(type(123))
+print(type(123.456))
+print(type(True))
+
+#Type Conversion - convert one data type into another
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
