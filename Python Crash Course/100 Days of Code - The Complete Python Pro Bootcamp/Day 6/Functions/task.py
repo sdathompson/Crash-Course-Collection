4b5538a3375d4ed18d70403e64ed6224bf7fff32
Shane Thompson <sda.thompson@hotmail.com>
2025-04-16 14:58:24 -0400

    Day 6 Almost Done

---
+++ Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 6/Functions/task.py
@@ -0,0 +1,40 @@
+def get_user_name():
+    name = input("What is your name?: \n")
+    print("Hello, " + name)
+
+get_user_name()
+
+
+
+# todo: Hurdle 4
+    # at_goal()
+    #
+    #
+    # def turn_right():
+    #     turn_left()
+    #     turn_left()
+    #     turn_left()
+    #
+    #
+    # def hurdle():
+    #     turn_left()
+    #     while wall_on_right():
+    #         move()
+    #     turn_right()
+    #     move()
+    #     turn_right()
+    #     move()
+    #     while wall_on_right() and front_is_clear():
+    #         move()
+    #     turn_left()
+    #
+    #
+    # while front_is_clear() and not at_goal():
+    #     move()
+    #     while wall_in_front():
+    #         hurdle()
+
+#todo: Lost in a Maze
+    # Follow along the right edge of the maze,
+    # turning right if it can, going straight ahead if it can't turn right,
+    # and turning left as a last resort
