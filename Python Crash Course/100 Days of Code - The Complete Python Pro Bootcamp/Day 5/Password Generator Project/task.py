3935935ac5a1b9c038f0a08cd3b63340ace6ea41
Shane Thompson <sda.thompson@hotmail.com>
2025-04-16 13:47:21 -0400

    Day 5 Done

--- Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 5/Password Generator Project/task.py
+++ Python Crash Course/100 Days of Code - The Complete Python Pro Bootcamp/Day 5/Password Generator Project/task.py
@@ -4,11 +4,6 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-rnd_letters = random.choice(letters).lower()
-rnd_numbers = random.choice(numbers)
-rnd_symbols = random.choice(symbols)
-
-
 
 
 print("Welcome to the PyPassword Generator!")
@@ -16,7 +11,21 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password = ""
 
+for char_l in range(1, nr_letters+1):
+    rnd_letters = str(random.choice(letters).lower())
+    password += rnd_letters
+for char_s in range(1, nr_symbols+1):
+    rnd_symbols = random.choice(symbols)
+    password += rnd_symbols
+for char_n in range(1, nr_numbers+1):
+    rnd_numbers = random.choice(numbers)
+    password += rnd_numbers
+
+password_shuffle = ''.join(random.sample(password,len(password)))
+
+print(f"Your generated password is: {password_shuffle}")
 
 
 
