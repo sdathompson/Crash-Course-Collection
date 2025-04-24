f1b478d9f86344e4e1879267c09af1f869dbf887
Shane Thompson <sda.thompson@hotmail.com>
2025-04-24 13:57:36 -0400

    Day 16 - OOP Coffee Machine

--- Python Crash Course/100Days/OOPCoffeeMachine/main.py
+++ Python Crash Course/100Days/OOPCoffeeMachine/main.py
@@ -1,3 +1,41 @@
+import sys
+
 from menu import Menu
+from menu import MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from sys import exit
+
+#TODO: Prompt user ("What would you like? (espresso/latte/cappuccino):")
+# Check user input to decide what to do next
+match_order = Menu()
+money_time = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
+
+def process():
+    print("Welcome to the Coffee Machine!")
+    coffee_order = input("What would you like? (espresso/latte/cappuccino): ")
+    enough_order = True
+
+    if coffee_order == "off":
+        print("Rebooting coffee machine...")
+        process()
+
+    if coffee_order == "report":
+        coffee_maker.report()
+        money_time.report()
+        exit()
+
+    for n_idx in match_order.menu:
+        # Check if there are enough resources to make the drink
+        enough_order = coffee_maker.is_resource_sufficient(n_idx)
+        if not enough_order:
+            process()
+
+    #Process coins
+    deposit = money_time.process_coins()
+    # Check if the transaction is successful
+    money_time.make_payment(deposit)
+    process()
+process()
