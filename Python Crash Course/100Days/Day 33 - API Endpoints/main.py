9b2e7793aa29c3412c78f723c42a7ac6efb9f2fc
Shane Thompson <sda.thompson@hotmail.com>
2026-06-11 01:17:55 -0400

    Day 33 - RS API

--- Python Crash Course/100Days/Day 33 - API Endpoints/main.py
+++ Python Crash Course/100Days/Day 33 - API Endpoints/main.py
@@ -12,11 +12,17 @@ GEID_response = requests.get(url="https://oldschool.runescape.wiki/?title=Module
 GEID_response.raise_for_status()
 GEID_data = GEID_response.json()
 
-search_input = input("What item do you want to get pricing for?: ")
-if search_input is not None:
-    search_id = GEID_data[search_input]
+def API_input():
+    search_input = input("What item do you want to get pricing for?: ")
+    try:
+        search_id = GEID_data[search_input]
+        return search_id
+    except KeyError or NameError:
+        print("Invalid input. Try again.")
+        return API_input()
 
-GEdetails_response = requests.get(url=f"https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item={search_id}")
+
+GEdetails_response = requests.get(url=f"https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item={API_input()}")
 GEdetails_response.raise_for_status()
 GEdetails_data = GEdetails_response.json()
 
